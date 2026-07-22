9a2467a23b2148281f0b345557970b58203c9d57
medfa12 <156374960+medfa12@users.noreply.github.com>
2026-07-21 21:36:42 -0400

    [net] Implement libSceNet/NetCtl gap NIDs: socket/resolver/epoll/config getters, NetCtl info/callback surface

--- src/SharpEmu.Libs/Network/NetCtlExports.cs
+++ src/SharpEmu.Libs/Network/NetCtlExports.cs
@@ -238,6 +238,190 @@ public static class NetCtlExports
         return ctx.SetReturn(0, typeof(long));
     }
 
+    private static bool _ipcConnected = true;
+    private static bool _bandwidthManagementEnabled;
+
+    [SysAbiExport(Nid = "UF6H6+kjyQs", ExportName = "sceNetCtlCheckCallbackForLibIpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlCheckCallbackForLibIpcInt(CpuContext ctx) => NetCtlOk(ctx);
+
+    [SysAbiExport(Nid = "vv6g8zoanL4", ExportName = "sceNetCtlClearEventForLibIpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlClearEventForLibIpcInt(CpuContext ctx) => NetCtlOk(ctx);
+
+    [SysAbiExport(Nid = "8OJ86vFucfo", ExportName = "sceNetCtlClearEventIpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlClearEventIpcInt(CpuContext ctx) => NetCtlOk(ctx);
+
+    [SysAbiExport(Nid = "HCD46HVTyQg", ExportName = "sceNetCtlConnectConfIpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlConnectConfIpcInt(CpuContext ctx) => SetIpcConnection(ctx, true);
+
+    [SysAbiExport(Nid = "ID+Gq3Ddzbg", ExportName = "sceNetCtlConnectIpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlConnectIpcInt(CpuContext ctx) => SetIpcConnection(ctx, true);
+
+    [SysAbiExport(Nid = "aPpic8K75YA", ExportName = "sceNetCtlConnectWithRetryIpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlConnectWithRetryIpcInt(CpuContext ctx) => SetIpcConnection(ctx, true);
+
+    [SysAbiExport(Nid = "9y4IcsJdTCc", ExportName = "sceNetCtlDisableBandwidthManagementIpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlDisableBandwidthManagementIpcInt(CpuContext ctx)
+    {
+        _bandwidthManagementEnabled = false;
+        return NetCtlOk(ctx);
+    }
+
+    [SysAbiExport(Nid = "qOefcpoSs0k", ExportName = "sceNetCtlDisconnectIpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlDisconnectIpcInt(CpuContext ctx) => SetIpcConnection(ctx, false);
+
+    [SysAbiExport(Nid = "x9bSmRSE+hc", ExportName = "sceNetCtlEnableBandwidthManagementIpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlEnableBandwidthManagementIpcInt(CpuContext ctx)
+    {
+        _bandwidthManagementEnabled = true;
+        return NetCtlOk(ctx);
+    }
+
+    [SysAbiExport(Nid = "eCUIlA2t5CE", ExportName = "sceNetCtlGetBandwidthInfoIpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlGetBandwidthInfoIpcInt(CpuContext ctx) => ClearOptional(ctx, ctx[CpuRegister.Rdi], 32);
+
+    [SysAbiExport(Nid = "2EfjPXVPk3s", ExportName = "sceNetCtlGetEtherLinkMode", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlGetEtherLinkMode(CpuContext ctx) => WriteRequiredUInt32(ctx, ctx[CpuRegister.Rdi], 1);
+
+    [SysAbiExport(Nid = "teuK4QnJTGg", ExportName = "sceNetCtlGetIfStat", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlGetIfStat(CpuContext ctx) => ClearRequired(ctx, ctx[CpuRegister.Rsi], 64);
+
+    [SysAbiExport(Nid = "xstcTqAhTys", ExportName = "sceNetCtlGetInfoIpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlGetInfoIpcInt(CpuContext ctx) => NetCtlGetInfo(ctx);
+
+    [SysAbiExport(Nid = "arAQRFlwqaA", ExportName = "sceNetCtlGetInfoV6IpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlGetInfoV6IpcInt(CpuContext ctx) => NetCtlGetInfo(ctx);
+
+    [SysAbiExport(Nid = "x+cnsAxKSHo", ExportName = "sceNetCtlGetNatInfoIpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlGetNatInfoIpcInt(CpuContext ctx) => NetCtlGetNatInfo(ctx);
+
+    [SysAbiExport(Nid = "hhTsdv99azU", ExportName = "sceNetCtlGetNetEvConfigInfoIpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlGetNetEvConfigInfoIpcInt(CpuContext ctx) => ClearOptional(ctx, ctx[CpuRegister.Rdi], 32);
+
+    [SysAbiExport(Nid = "0cBgduPRR+M", ExportName = "sceNetCtlGetResult", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlGetResult(CpuContext ctx) => WriteRequiredUInt32(ctx, ctx[CpuRegister.Rsi], 0);
+
+    [SysAbiExport(Nid = "NEtnusbZyAs", ExportName = "sceNetCtlGetResultIpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlGetResultIpcInt(CpuContext ctx) => NetCtlGetResult(ctx);
+
+    [SysAbiExport(Nid = "vdsTa93atXY", ExportName = "sceNetCtlGetResultV6IpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlGetResultV6IpcInt(CpuContext ctx) => NetCtlGetResult(ctx);
+
+    [SysAbiExport(Nid = "wP0Ab2maR1Y", ExportName = "sceNetCtlGetScanInfoBssidForSsidListScanIpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlGetScanInfoBssidForSsidListScanIpcInt(CpuContext ctx) => ClearScanResult(ctx);
+
+    [SysAbiExport(Nid = "Wn-+887Lt2s", ExportName = "sceNetCtlGetScanInfoBssidIpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlGetScanInfoBssidIpcInt(CpuContext ctx) => ClearScanResult(ctx);
+
+    [SysAbiExport(Nid = "FEdkOG1VbQo", ExportName = "sceNetCtlGetScanInfoByBssidIpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlGetScanInfoByBssidIpcInt(CpuContext ctx) => ClearScanResult(ctx);
+
+    [SysAbiExport(Nid = "irV8voIAHDw", ExportName = "sceNetCtlGetScanInfoForSsidListScanIpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlGetScanInfoForSsidListScanIpcInt(CpuContext ctx) => ClearScanResult(ctx);
+
+    [SysAbiExport(Nid = "L97eAHI0xxs", ExportName = "sceNetCtlGetScanInfoForSsidScanIpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlGetScanInfoForSsidScanIpcInt(CpuContext ctx) => ClearScanResult(ctx);
+
+    [SysAbiExport(Nid = "JXlI9EZVjf4", ExportName = "sceNetCtlGetState2IpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlGetState2IpcInt(CpuContext ctx) => WriteIpcState(ctx);
+
+    [SysAbiExport(Nid = "gvnJPMkSoAY", ExportName = "sceNetCtlGetStateIpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlGetStateIpcInt(CpuContext ctx) => WriteIpcState(ctx);
+
+    [SysAbiExport(Nid = "O8Fk4w5MWss", ExportName = "sceNetCtlGetStateV6IpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlGetStateV6IpcInt(CpuContext ctx) => WriteIpcState(ctx);
+
+    [SysAbiExport(Nid = "BXW9b3R1Nw4", ExportName = "sceNetCtlGetWifiType", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlGetWifiType(CpuContext ctx) => WriteRequiredUInt32(ctx, ctx[CpuRegister.Rdi], 0);
+
+    [SysAbiExport(Nid = "YtAnCkTR0K4", ExportName = "sceNetCtlIsBandwidthManagementEnabledIpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlIsBandwidthManagementEnabledIpcInt(CpuContext ctx) =>
+        WriteRequiredUInt32(ctx, ctx[CpuRegister.Rdi], _bandwidthManagementEnabled ? 1U : 0U);
+
+    [SysAbiExport(Nid = "WRvDk2syatE", ExportName = "sceNetCtlRegisterCallbackForLibIpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlRegisterCallbackForLibIpcInt(CpuContext ctx) => NetCtlRegisterCallback(ctx);
+
+    [SysAbiExport(Nid = "rqkh2kXvLSw", ExportName = "sceNetCtlRegisterCallbackIpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlRegisterCallbackIpcInt(CpuContext ctx) => NetCtlRegisterCallback(ctx);
+
+    [SysAbiExport(Nid = "ipqlpcIqRsQ", ExportName = "sceNetCtlRegisterCallbackV6IpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlRegisterCallbackV6IpcInt(CpuContext ctx) => NetCtlRegisterCallback(ctx);
+
+    [SysAbiExport(Nid = "reIsHryCDx4", ExportName = "sceNetCtlScanIpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlScanIpcInt(CpuContext ctx) => NetCtlOk(ctx);
+
+    [SysAbiExport(Nid = "LJYiiIS4HB0", ExportName = "sceNetCtlSetErrorNotificationEnabledIpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlSetErrorNotificationEnabledIpcInt(CpuContext ctx) => NetCtlOk(ctx);
+
+    [SysAbiExport(Nid = "DjuqqqV08Nk", ExportName = "sceNetCtlSetStunWithPaddingFlagIpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlSetStunWithPaddingFlagIpcInt(CpuContext ctx) => NetCtlOk(ctx);
+
+    [SysAbiExport(Nid = "urWaUWkEGZg", ExportName = "sceNetCtlUnregisterCallbackForLibIpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlUnregisterCallbackForLibIpcInt(CpuContext ctx) => NetCtlUnregisterCallback(ctx);
+
+    [SysAbiExport(Nid = "by9cbB7JGJE", ExportName = "sceNetCtlUnregisterCallbackIpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlUnregisterCallbackIpcInt(CpuContext ctx) => NetCtlUnregisterCallback(ctx);
+
+    [SysAbiExport(Nid = "Hjxpy28aID8", ExportName = "sceNetCtlUnregisterCallbackV6IpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlUnregisterCallbackV6IpcInt(CpuContext ctx) => NetCtlUnregisterCallback(ctx);
+
+    [SysAbiExport(Nid = "1HSvkN9oxO4", ExportName = "sceNetCtlUnsetStunWithPaddingFlagIpcInt", Target = Generation.Gen5, LibraryName = "libSceNetCtl")]
+    public static int NetCtlUnsetStunWithPaddingFlagIpcInt(CpuContext ctx) => NetCtlOk(ctx);
+
+    internal static void ResetIpcStateForTests()
+    {
+        _ipcConnected = true;
+        _bandwidthManagementEnabled = false;
+        lock (CallbackGate)
+        {
+            Array.Clear(Callbacks);
+        }
+    }
+
+    private static int SetIpcConnection(CpuContext ctx, bool connected)
+    {
+        _ipcConnected = connected;
+        return NetCtlOk(ctx);
+    }
+
+    private static int WriteIpcState(CpuContext ctx) =>
+        WriteRequiredUInt32(ctx, ctx[CpuRegister.Rdi], _ipcConnected ? NetCtlStateIpObtained : 0U);
+
+    private static int ClearScanResult(CpuContext ctx)
+    {
+        if (ctx[CpuRegister.Rdx] != 0 && !ctx.TryWriteUInt32(ctx[CpuRegister.Rdx], 0))
+        {
+            return ctx.SetReturn((int)OrbisGen2Result.ORBIS_GEN2_ERROR_MEMORY_FAULT, typeof(long));
+        }
+
+        return ClearOptional(ctx, ctx[CpuRegister.Rsi], 256);
+    }
+
+    private static int ClearRequired(CpuContext ctx, ulong address, int byteCount)
+    {
+        if (address == 0)
+        {
+            return ctx.SetReturn(NetCtlErrorInvalidAddress, typeof(long));
+        }
+
+        return ClearOptional(ctx, address, byteCount);
+    }
+
+    private static int ClearOptional(CpuContext ctx, ulong address, int byteCount)
+    {
+        if (address == 0)
+        {
+            return NetCtlOk(ctx);
+        }
+
+        return WriteBytes(ctx, address, new byte[byteCount]);
+    }
+
+    private static int WriteRequiredUInt32(CpuContext ctx, ulong address, uint value) => address == 0
+        ? ctx.SetReturn(NetCtlErrorInvalidAddress, typeof(long))
+        : WriteUInt32(ctx, address, value);
+
+    private static int NetCtlOk(CpuContext ctx) => ctx.SetReturn(0, typeof(long));
+
     private static int WriteBytes(CpuContext ctx, ulong address, ReadOnlySpan<byte> bytes) =>
         ctx.Memory.TryWrite(address, bytes)
             ? ctx.SetReturn(0, typeof(long))
